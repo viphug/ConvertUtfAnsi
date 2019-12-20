d7b9430ac5fdb1901c2377057c0d01891f0aaa73
viphug <44405287+viphug@users.noreply.github.com>
2019-12-20 14:42:28 -0300

    Create ConvertUtfAnsi.py

---
+++ ConvertUtfAnsi.py
@@ -0,0 +1,19 @@
+#Imports
+import unidecode
+
+ini = #Caminho completo do arquivo
+
+#Abre o arquivo indicado
+arq = open(ini,'r',encoding='utf-8').readlines()
+
+#Remove caracteres especiais
+dec = [unidecode.unidecode(linhas) for linhas in arq]
+
+#Cria novo arquivo no formato ANSI
+arq2 = open(ini.replace('.csv','_U.txt'), 'w',encoding='iso-8859-1')
+
+#Escreve as linhas no novo arquivo
+arq2.writelines(dec)
+
+#Fecha arquivo
+arq2.close()
